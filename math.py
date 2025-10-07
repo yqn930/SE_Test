939acb25247853fe7291630265d5eac3ae634d38
ruby-1009 <ruby.93109@gmail.com>
2025-10-07 10:52:35 +0800

    Update math.py

--- math.py
+++ math.py
@@ -1,13 +1,18 @@
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
-choice = input("Enter choice (+,-,*,/): ")
+choice = input("Enter choice (+, /): ")
+
+def add(a, b):
+    return a + b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Cannot divide by zero!"
+    else:
+        return a / b
 
 if choice == '+':
     print(f"Result: {add(num1, num2)}")
-elif choice == '-':
-    print(f"Result: {subtract(num1, num2)}")
-elif choice == '*':
-    print(f"Result: {multiply(num1, num2)}")
 elif choice == '/':
     print(f"Result: {divide(num1, num2)}")
 else:
