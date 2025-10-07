8ce4c0d2a8b6cc238c084d1dd08501fced707e97
HANSON\hanso <hanson0509@gmail.com>
2025-10-07 10:59:48 +0800

    新增減法函數

--- math.py
+++ math.py
@@ -2,6 +2,9 @@ num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 choice = input("Enter choice (+,-,*,/): ")
 
+def subtract(a, b):
+    return a - b
+
 if choice == '+':
     print(f"Result: {add(num1, num2)}")
 elif choice == '-':
